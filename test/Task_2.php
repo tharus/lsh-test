93fce368ef3e9c619469ef2569cb056caf021e48
tharus <tmsdahanayake@gmail.com>
2023-01-18 17:58:56 +0530

    Update Task_2.php
    added comments

--- test/Task_2.php
+++ test/Task_2.php
@@ -2,16 +2,19 @@
 
 // I am not using PHP built in functions here
 
-
 function reformat($sentence) {
     $vowels = array("A", "E", "I", "O", "U", "a", "e", "i", "o", "u");
     $output = "";
     $first_letter = true;
 
+    //loo through the sentence
     for ($i = 0; $i < strlen($sentence); $i++) {
+        //check whether vowel is their
         if (in_array($sentence[$i], $vowels)) {
             continue;
         }
+
+        //make first letter capital and others simple
         if ($first_letter) {
             $output .= strtoupper($sentence[$i]);
             $first_letter = false;
